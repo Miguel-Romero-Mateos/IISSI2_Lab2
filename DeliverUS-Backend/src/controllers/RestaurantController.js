2f42730ada7a42e21570443adb01216690c88726
MIGUEL ROMERO <migrommat@alum.us.es>
2026-03-11 23:06:57 +0100

    Laboratorio 2 - Backend DONE & PASSED

--- DeliverUS-Backend/src/controllers/RestaurantController.js
+++ DeliverUS-Backend/src/controllers/RestaurantController.js
@@ -1,5 +1,6 @@
 import { Restaurant, Product, RestaurantCategory, ProductCategory } from '../models/models.js'
 
+//Create method to read all restaurants:
 const index = async function (req, res) {
   try {
     const restaurants = await Restaurant.findAll(
@@ -21,19 +22,58 @@ const index = async function (req, res) {
 
 // TODO: Complete the following functions
 
+//Create method to create a new restaurant:
 const create = async function (req, res) {
-
+  const newRestaurant = Restaurant.build(req.body)
+  newRestaurant.userId = 1
+  try{
+    const restaurant = await newRestaurant.save()
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
+//Create method to read a restaurant by id:
 const show = async function (req, res) {
-
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.id, {
+      attributes: { exclude: ['userId'] },
+      include: [{
+        model: Product,
+        as: 'products',
+        include: {model: ProductCategory, as: 'productCategory'}
+      },
+    {
+      model: RestaurantCategory,
+      as: 'restaurantCategory'
+    }],
+    order: [[{model:Product, as: 'products'}, 'order', 'ASC']],
+    })
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const update = async function (req, res) {
-
+  const restaurant = await Restaurant.findByPk(req.params.id)
+  try {
+    const updatedRestaurant = await restaurant.update(req.body)
+    res.json(updatedRestaurant)
+  } catch (err){
+    res.status(500).send(err)
+  }
 }
 
 const destroy = async function (req, res) {
+  const restaurant = await Restaurant.findByPk(req.params.id)
+  try {
+    await restaurant.destroy()
+    res.json("Restaurant deleted successfully")
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const RestaurantController = {
--- DeliverUS-Backend/src/routes/RestaurantRoutes.js
+++ DeliverUS-Backend/src/routes/RestaurantRoutes.js
@@ -1,7 +1,15 @@
 import RestaurantController from '../controllers/RestaurantController.js'
 
 const loadFileRoutes = function (app) {
-  // TODO: Include routes for restaurant described in the lab session README
+
+  app.route('/restaurants')
+    .get(RestaurantController.index) //Read all restaurants
+    .post(RestaurantController.create) //Create a new restaurant
+
+  app.route('/restaurants/:id')
+    .get(RestaurantController.show) //Read a restaurant by id
+    .put(RestaurantController.update) //Update a restaurant by id
+    .delete(RestaurantController.destroy) //Delete a restaurant by id
 
 }
 export default loadFileRoutes
